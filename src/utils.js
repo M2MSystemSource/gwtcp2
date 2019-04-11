da1d85a7cb2fb6dba719dfec9f20bed28e99a88d
Jordi Enguídanos <jenguidanos@imasdtecnologia.com>
2019-04-11 13:41:01 +0200

    parse gsm time (gtime)

--- src/utils.js
+++ src/utils.js
@@ -2,23 +2,38 @@ module.exports = (app) => {
   const self = {}
 
   /**
-   * Recibe una fecha en formato simcom y lo devuelve como unix timestamp GMT0
-   * Formato de entrada: YYYYMMDDHHYYSS.mmm
+   * Recibe una fecha en formato simcom GPS y lo devuelve como unix timestamp
+   * GMT0. Formato de entrada: YYYYMMDDHHYYSS.mmm
    *
-   * En principio el simcom obtiene las fechas en formato GMT0, utilizamos
-   * el método `Date.UTC()` para convertirlo en timestamp GMT+0
+   * Esta es la fecha GPS obtenida con el comando "AT+CGNSINF". No confundir con
+   * la fecha GSM obtenida con el comando "AT+CCLK?"
    *
-   * @param {String} data
+   * @param {String} date YYYYMMDDhhmmss.zzz
    */
-  self.simcomDateTimeToTimestamp = (data) => {
-    const year = parseInt(data.substr(0, 4), 10)
-    const month = parseInt(data.substr(4, 2), 10)
-    const day = parseInt(data.substr(6, 2), 10)
-    const hours = parseInt(data.substr(8, 2), 10)
-    const minutes = parseInt(data.substr(10, 2), 10)
-    const seconds = parseInt(data.substr(12, 2), 10)
+  self.simcomDateTimeToTimestamp = (date) => {
+    const year = parseInt(date.substr(0, 4), 10)
+    const month = parseInt(date.substr(4, 2), 10)
+    const day = parseInt(date.substr(6, 2), 10)
+    const hours = parseInt(date.substr(8, 2), 10)
+    const minutes = parseInt(date.substr(10, 2), 10)
+    const seconds = parseInt(date.substr(12, 2), 10)
 
     return Date.UTC(year, month - 1, day, hours, minutes, seconds)
+  }
+
+  self.simcomGSMDateTimeToTimestamp = (date) => {
+    const year = parseInt(date.substr(0, 2), 10) + 2000
+    const month = date.substr(2, 2)
+    const day = date.substr(4, 2)
+    const hours = date.substr(6, 2)
+    const minutes = date.substr(8, 2)
+    const seconds = date.substr(10, 2)
+    const timezone = date.substr(12, 2) / 4
+
+    const dateString = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}-0200`
+    console.log(dateString)
+    const d = new Date(dateString)
+    return d
   }
 
   /**
